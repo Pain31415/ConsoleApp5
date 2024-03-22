bdf825ce417784b3c8b69c3639ba3b1967cb875d
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { -2, -5, -2, 3, -1, 3, -1, 0 };
+
+            Func<int[], int[]> uniqueNegativeNumbers = nums => nums.Where(num => num < 0).Distinct().ToArray();
+
+            int[] uniqueNegatives = uniqueNegativeNumbers(numbers);
+
+            Console.WriteLine("Унікальні, негативні числа у масиві:");
+            foreach (int num in uniqueNegatives)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
