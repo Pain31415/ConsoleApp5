05380924ea622ead49a8180ca268647f79f40c6c
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 3, 7, 2, 8, 1, 5 };
+
+            Func<int[], int> findMin = arr =>
+            {
+                int min = arr[0];
+                foreach (int num in arr)
+                {
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                }
+                return min;
+            };
+
+            Console.WriteLine($"Мінімальний елемент в масиві: {findMin(array)}");
+        }
+    }
+}
