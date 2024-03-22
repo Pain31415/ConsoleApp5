fe84ac4fbabdd0e79bde62a490d94f49126541b6
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            int[] array = { 3, 7, 2, 8, 1, 5 };
+
+            Func<int[], Task<int[]>> findOddNumbersAsync = async arr =>
+            {
+                await Task.Delay(100);
+                return arr.Where(num => num % 2 != 0).ToArray();
+            };
+
+            int[] oddNumbers = await findOddNumbersAsync(array);
+
+            Console.WriteLine("Непарні числа в масиві:");
+            foreach (int num in oddNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
