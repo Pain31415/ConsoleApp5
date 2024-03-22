ee1a41b7abc5fcc7af1c52a65e4b639cfe86a4de
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string keyword = "машини";
+            string textToCheck = "У нас є багато машин і мотоциклів.";
+
+            Func<string, string, bool> checkTextForWord = (text, word) => text.ToLower().Contains(word.ToLower());
+
+            bool containsWord = checkTextForWord(textToCheck, keyword);
+
+            Console.WriteLine($"Текст містить слово \"{keyword}\": {containsWord}");
+        }
+    }
+}
