903254f34464f2768184a94bfd6cb3bf9c80ca4b
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { -2, 5, -8, 10, 3, -1, 0 };
+
+            Func<int[], int> countPositiveNumbers = nums => nums.Count(num => num > 0);
+
+            int count = countPositiveNumbers(numbers);
+
+            Console.WriteLine($"Кількість позитивних чисел у масиві: {count}");
+        }
+    }
+}
