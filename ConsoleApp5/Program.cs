504b9b11659e9ab4e71216774f2c185badc5e279
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Func<int, Task<int>> calculateSquareAsync = async (int number) =>
+            {
+                await Task.Delay(100);
+                return number * number;
+            };
+
+            int testNumber = 5;
+            int square = await calculateSquareAsync(testNumber);
+            Console.WriteLine($"Квадрат числа {testNumber} дорівнює: {square}");
+        }
+    }
+}
