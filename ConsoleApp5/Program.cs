eea02c4343b4360463825fe4eaa11da0a3b5e04a
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            int[] numbers = { 7, 14, 21, 3, 5, 6, 28 };
+
+            Func<int[], Task<int>> countNumbersDivisibleBySevenAsync = async nums =>
+            {
+                await Task.Delay(100);
+                return nums.Count(num => num % 7 == 0);
+            };
+
+            int count = await countNumbersDivisibleBySevenAsync(numbers);
+
+            Console.WriteLine($"Кількість чисел у масиві, кратних семи: {count}");
+        }
+    }
+}
