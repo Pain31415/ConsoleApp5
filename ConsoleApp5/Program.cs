310a29d2232b25483572ebab5e2506e78ba4ac14
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, double> calculateCube = number =>
+            {
+                double square = Math.Pow(number, 2);
+                return square * number;
+            };
+
+            int testNumber = 4;
+            Console.WriteLine($"Куб числа {testNumber} дорівнює: {calculateCube(testNumber)}");
+        }
+    }
+}
