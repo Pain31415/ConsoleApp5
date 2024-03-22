fdbe3d839690420d58838b1e3f7b03f85292f18b
Pain31415 <Cherniy412@ukr.net>
2024-03-22 19:39:10 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, bool> checkEven = delegate (int number)
+            {
+                return number % 2 == 0;
+            };
+
+            int testNumber = 8;
+            Console.WriteLine($"Число {testNumber} парне: {checkEven(testNumber)}");
+
+            testNumber = 9;
+            Console.WriteLine($"Число {testNumber} парне: {checkEven(testNumber)}");
+        }
+    }
+}
